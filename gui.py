f845f74621f7e1bb628c77647e9ede212cbc0576
Noah Langat <Noah.Langat@lumentum.com>
2018-05-30 16:27:44 -0400

    Nothing important.

--- gui.py
+++ gui.py
@@ -1,48 +1,64 @@
 import sys
-from PyQt5 import QtGui, QtCore, QtWidget
-from PyQt5.QtWidgets import QApplication, QWidget
-import main
+from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication
+# import main
 
 
-class Window(QtGui.QWindow):
+class Window(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        menubar = self.menuBar()
+        menubar.addMenu('File')
         self.setGeometry(50, 50, 500, 300)
-        self.setTitle("PyQt5 Trail")
-        self.setIcon(QtGui.QIcon('BlackPanther.png'))
-        self.equationBox()
-        self.sampleCalcBox()
-        self.latexOutputBox()
+        self.setWindowTitle("PyQt5 Trial")
 
-    # def equationBox(self):
-    #     print('To be implemented')
+    def addEquationBox(self):
+        self.EquationBox = equationBox((80, 20))
 
-    # def latexOutputBox(self):
-    #     print('To be implemented')
+    def addOutput(self, position):
+        latexOutputBox((150, 100))
 
-class equationBox(QtWidget.QLineEdit):
+class secondaryWindow(QtGui.QWindow):
+    def __init__(self):
+        super().__init__()
+        self.setBaseSize()
+        self.setTitle("Sample Calculation")
+
+    def addSampleCalcBox(self, position):
+        equationBox(position)
+        
+class equationBox(QtWidgets.QLineEdit):
     def __init__(self, position):
         super().__init__()
+        self.setPlaceholderText("Equation")
+        self.move(position[0], position[1])
+        #self.show()
         #Continue class definition
 
 
-class latexOutputBox(QtWidget.QTextBox):
+class latexOutputBox(QtWidgets.QTextEdit):
     def __init__(self, position):
         super().__init__()
+        self.setPlaceholderText("LaTex OutPut")
+        self.move(position[0], position[1])
+        #self.move()
         #Continue class definition
 
 
-class sampleCalculation(QWidget.QLineEdit):
+class sampleCalculation(QtWidgets.QLineEdit):
 
     def __init__(self, position, boxTitle):
         super().__init__()
         self.move(position[0], position[1])
         self.setTitle(boxTitle)
+        self.setBaseSize(50, 50)
 
 
 def run():
-    app = QtGui.QGuiApplication(sys.argv)
-    myWindow = Window() 
+    app = QApplication(sys.argv)
+    myWindow = Window()
+    myWindow.addEquationBox()
+    myWindow.show()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':

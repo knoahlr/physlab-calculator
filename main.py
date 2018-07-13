0a06e90b570999fe748a44c8bfce5f933aaf3196
NoahWorkstation <noahlangat@cmail.carleton.ca>
2018-07-13 01:08:18 -0400

    File restructuring

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from gui import mainWindow, secondaryWindow
+import sys, time, ctypes, re
+from mainWindow import mainWindow
+from secondaryWindow import secondaryWindow
+
+from PyQt5.QtWidgets import QCommonStyle, QApplication
 
 if __name__ == '__main__':
-    #StartApplication
+    
+    myappid = 'mycompany.myproduct.subproduct.version' # arbitrary string
+    ''' https://stackoverflow.com/questions/1551605/how-to-set-applications-taskbar-icon-in-windows-7/1552105#1552105 '''
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
-    guiApp = QApplication(sys.argv)
+    app = QApplication(sys.argv)
     app.setStyle(QCommonStyle())
 
+
     window = mainWindow()
+    window.show()
+    sys.exit(app.exec_())
